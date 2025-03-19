bab51f3e4a70cd4c542f7e13174a2ec49b4ca415
anshu00001git <infraredevil12345@gmail.com>
2025-03-19 10:17:05 +0530

    Update adbms_git_sql.py

--- adbms_git_sql.py
+++ adbms_git_sql.py
@@ -1,1 +1,11 @@
 import mysql.connector as myconn
+print(myconn.__version__)
+
+
+# Connect to the MySQL server
+conn = myconn.connect(
+    host="localhost",  # Replace with your MySQL host
+    user="sudip",       # Replace with your MySQL username
+    password="sudip@123",
+    database="Anshu" # Replace with your MySQL password
+)
